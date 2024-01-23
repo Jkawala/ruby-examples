e6fa17e01352c4abcb14eb89bdcaebad976642d2
Jkawala <91265409+Jkawala@users.noreply.github.com>
2024-01-23 01:00:14 +0000

    Add files via upload

---
+++ Ruby on Rails Lab/ex1.rb
@@ -0,0 +1,8 @@
+puts " Hello World"
+#The "puts" method prints out the variable
+puts  (" Enter your name")
+#asking the user to input a variable
+name = gets()
+#gets the user information which is the name
+puts ("Hello #{name}")
+#the output is the users name 
